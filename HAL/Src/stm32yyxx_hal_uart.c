f332e3b3457fb437edb143f8915d2df5c9bbb7fe
NextGenXR <jenni@nextgenxr.com>
2023-06-26 07:13:28 -0500

    Change to #elif defined(yyxx)

--- HAL/Src/stm32yyxx_hal_uart.c
+++ HAL/Src/stm32yyxx_hal_uart.c
@@ -6,39 +6,41 @@
     #include <main.h>
 #endif
 
+
+
 #ifdef STM32F0xx
   #include <stm32f0xx_hal_uart.c>
-#elif STM32F1xx
+#elif defined(STM32F1xx)
   #include <stm32f1xx_hal_uart.c>
-#elif STM32F2xx
+#elif defined(STM32F2xx)
   #include <stm32f2xx_hal_uart.c>
-#elif STM32F3xx
+#elif defined(STM32F3xx)
   #include <stm32f3xx_hal_uart.c>
-#elif STM32F4xx
+#elif defined(STM32F4xx)
   #include <stm32f4xx_hal_uart.c>
-#elif STM32F7xx
+#elif defined(STM32F7xx)
   #include <stm32f7xx_hal_uart.c>
-#elif STM32G0xx
+#elif defined(STM32G0xx)
   #include <stm32g0xx_hal_uart.c>
-#elif STM32G4xx
+#elif defined(STM32G4xx)
   #include <stm32g4xx_hal_uart.c>
-#elif STM32H7xx
+#elif defined(STM32H7xx)
   #include <stm32h7xx_hal_uart.c>
-#elif STM32L0xx
+#elif defined(STM32L0xx)
   #include <stm32l0xx_hal_uart.c>
-#elif STM32L1xx
+#elif defined(STM32L1xx)
   #include <stm32l1xx_hal_uart.c>
-#elif defined(STM32L4xx)
+#elif defined(STM32L4xx))
   #include <stm32l4xx_hal_uart.c>
-#elif STM32L5xx
+#elif defined(STM32L5xx)
   #include <stm32l5xx_hal_uart.c>
-#elif STM32MP1xx
+#elif defined(STM32MP1xx)
   #include <stm32mp1xx_hal_uart.c>
-#elif STM32U5xx
+#elif defined(STM32U5xx)
   #include <stm32u5xx_hal_uart.c>
-#elif STM32WBxx
+#elif defined(STM32WBxx)
   #include <stm32wbxx_hal_uart.c>
-#elif STM32WLxx
+#elif defined(STM32WLxx)
   #include <stm32wlxx_hal_uart.c>
 #endif
 #pragma GCC diagnostic pop
