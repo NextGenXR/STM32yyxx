eb9b3f6f63a9d67929129e68c4bc2d223a592e5d
NextGenXR <jenni@nextgenxr.com>
2023-12-10 17:15:29 -0600

    Fix elif with extra parenthesis

--- HAL/Src/stm32yyxx_hal_uart.c
+++ HAL/Src/stm32yyxx_hal_uart.c
@@ -30,7 +30,8 @@
   #include <stm32l0xx_hal_uart.c>
 #elif defined(STM32L1xx)
   #include <stm32l1xx_hal_uart.c>
-#elif defined(STM32L4xx))
+#elif defined(STM32L4xx)
+
   #include <stm32l4xx_hal_uart.c>
 #elif defined(STM32L5xx)
   #include <stm32l5xx_hal_uart.c>
